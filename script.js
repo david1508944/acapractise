3da242fe49dbd59bb24c8b1f88287d23301666b7
unknown <davit.mazmanyan.y@tumo.org>
2022-09-02 18:59:48 +0400

    hello

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const text = document.querySelector(".texts");
-const loader = document.querySelector("#loading")
-const loader1 = document.getElementById('loading')
-
-function displayLoadin(){
-  loader.classList.add('display')
+const finds = document.querySelector(".finds");
+const loader = document.querySelector("#loading");
+const loader1 = document.getElementById("loading");
+console.log(document.getElementById("search").value);
+function displayLoadin() {
+  loader.classList.add("display");
 }
-function hideLoad(){
-  loader.classList.remove('display')
-  loader1.remove()
+function hideLoad() {
+  loader.classList.remove("display");
+  loader1.remove();
 }
-
-const getAndRenderData = () => {
-  displayLoadin()
+function search(id = document.getElementById("search").value) {
+  text.remove();
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((el) => el.json())
     .then((el) =>
       el.forEach((element) => {
-  hideLoad()
+        console.log(element.userId);
+        if (element.userId === document.getElementById("search").value) {
+          hideLoad();
+          const il = document.createElement("li");
+          il.classList.add("search");
+          il.innerHTML = `
+      <div class="padd userId">${element.userId}</div>
+      <div class="padd id">${element.id}</div>
+      <div class="color title">${element.title}</div>
+      <div class=" body">${element.body}</div>
+      `;
+          finds.append(il);
+        }
+      })
+    );
+    console.log(document.getElementById("search").value);
+}
+console.log(document.getElementById("search").value);
+const getAndRenderData = () => {
+  displayLoadin();
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((el) => el.json())
+    .then((el) =>
+      el.forEach((element) => {
+        hideLoad();
         const li = document.createElement("li");
-        li.classList.add("list"); 
+        li.classList.add("list");
         li.innerHTML = `
     <div class="padd userId">${element.userId}</div>
     <div class="padd id">${element.id}</div>
@@ -30,5 +54,5 @@ const getAndRenderData = () => {
     );
 };
 
-const button= document.getElementById('button')
-button.addEventListener("click", getAndRenderData) 
+const button = document.getElementById("button");
+button.addEventListener("click", getAndRenderData);
